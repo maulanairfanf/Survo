f420da13bb639d666599c76164724b4e86419f34
maulanairfanf <maulanairfanf@gmail.com>
2022-02-25 23:53:30 +0700

    initialize setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'assets/css/style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-blue-600">Haii</p>
+    </div>
+  );
+}
+
+export default App;
